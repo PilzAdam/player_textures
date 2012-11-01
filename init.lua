4e4e51057008fcc61d050deb52eba9df27ae9312
PilzAdam <adam-k@outlook.com>
2012-11-01 17:07:54 +0100

    Prefix player_ for textures

--- init.lua
+++ init.lua
@@ -1,8 +1,8 @@
 minetest.register_on_joinplayer(function(player)
-	local filename = minetest.get_modpath("player_textures").."/textures/"..player:get_player_name()
+	local filename = minetest.get_modpath("player_textures").."/textures/player_"..player:get_player_name()
 	if io.open(filename..".png") then
 		player:set_properties({
-			textures = {player:get_player_name()..".png", player:get_player_name().."_back.png"},
+			textures = {"player_"..player:get_player_name()..".png", "player_"..player:get_player_name().."_back.png"},
 		})
 	end
 end)
