d5a2a6e575858897283c00484a129a312249000a
sdzen <webmaster@sdzen.zzn.com>
2012-11-25 15:58:45 -0500

    alter to work with 3D player
    just a minor change.

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ minetest.register_on_joinplayer(function(player)
 	local filename = minetest.get_modpath("player_textures").."/textures/player_"..player:get_player_name()
 	if io.open(filename..".png") then
 		player:set_properties({
-			textures = {"player_"..player:get_player_name()..".png", "player_"..player:get_player_name().."_back.png"},
+			textures = {"player_"..player:get_player_name()..".png"},
 		})
 	end
 end)
