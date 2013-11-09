518830458a590acb65275243389f06ca48cf38f1
PilzAdam <pilzadam@minetest.net>
2013-11-09 15:16:37 +0100

    Update to new default functions

--- init.lua
+++ init.lua
@@ -1,8 +1,6 @@
 minetest.register_on_joinplayer(function(player)
 	local filename = minetest.get_modpath("player_textures").."/textures/player_"..player:get_player_name()
 	if io.open(filename..".png") then
-		player:set_properties({
-			textures = {"player_"..player:get_player_name()..".png", "player_"..player:get_player_name().."_back.png"},
-		})
+		default.player_set_textures(player, {"player_"..player:get_player_name()..".png"})
 	end
 end)
