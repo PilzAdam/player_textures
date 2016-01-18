38cb21851a6b4a3467a5796bafa7b957c2fcb329
Pinky Snow <thepinkysnowman@gmail.com>
2016-01-17 19:34:41 -0500

    updated to work with newer format of mc skins
    it will load the newer skins in the same way as the old ones, some newer mc skins may be lacking some details but atleast they won't be all messed up!

--- init.lua
+++ init.lua
@@ -3,6 +3,6 @@ minetest.register_on_joinplayer(function(player)
 	local f = io.open(filename..".png")
 	if f then
 		f:close()
-		default.player_set_textures(player, {"player_"..player:get_player_name()..".png"})
+		default.player_set_textures(player, {"[combine:64x32:0,0=player_"..player:get_player_name()..".png:"})
 	end
 end)
